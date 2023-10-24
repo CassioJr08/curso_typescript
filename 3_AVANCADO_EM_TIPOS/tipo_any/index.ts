ab751aa389a79c89ded1d490b20baa4e67599cb4
CassioJr08 <cassiojr0108@gmail.com>
2023-10-24 13:54:51 -0300

    delete

---
+++ 3_AVANCADO_EM_TIPOS/tipo_any/index.ts
@@ -0,0 +1,20 @@
+// 1- arrays
+
+let number: number[] = [1,2,3,4,5]
+number.push(10)
+
+console.log(number[5])
+
+const nomes: string[] = ['Cassio', 'Junior']
+
+nomes.push('silva')
+console.log(nomes)
+
+
+// 2 - outra sintaxe array
+const nums: Array<number> = [100, 200]
+
+nums.push(300)
+console.log(nums[2]);
+
+let boll: boolean[] = [true, false]
---
+++ 3_AVANCADO_EM_TIPOS/tipos_array/index.js
@@ -0,0 +1,11 @@
+// 1- arrays
+var number = [1, 2, 3, 4, 5];
+number.push(10);
+console.log(number[5]);
+var nomes = ['Cassio', 'Junior'];
+nomes.push('silva');
+console.log(nomes);
+// 2 - outra sintaxe array
+var nums = [100, 200];
+nums.push(300);
+console.log(nums[2]);
